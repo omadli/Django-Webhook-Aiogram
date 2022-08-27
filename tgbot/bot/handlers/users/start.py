098b6ba819aa68ab816eea332b0cb333a6d6a2ed
khasanovmma <khasanovmma010@gmail.com>
2022-08-27 19:19:05 +0500

    fix minore

--- tgbot/bot/handlers/users/start.py
+++ tgbot/bot/handlers/users/start.py
@@ -6,7 +6,7 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from tgbot.bot.loader import dp
 
 
-@dp.message_handler(CommandStart(), state=None)
+@dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
     
--- tgbot/bot/loader.py
+++ tgbot/bot/loader.py
@@ -1,7 +1,6 @@
 from src.settings import API_TOKEN
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
-from aiogram.contrib.fsm_storage.mongo import MongoStorage
 
 
 bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
